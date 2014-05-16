4ca6ab8bc409b2135cfb4a64af0668154806f585
rdpeake <rdpeake@adam.com.au>
2014-05-16 15:19:38 +0930

    Pre-build autocommit: Build Successful

--- Russell Peake Project/Russell Peake Project/Game1.cs
+++ Russell Peake Project/Russell Peake Project/Game1.cs
@@ -139,6 +139,7 @@ namespace Russell_Peake_Project
             MiniMap.MaxPitch = MathHelper.ToRadians(89.9f);
 
             //load machine room;
+            machine.initRoom();
             machine.init();
         }
 
