77f0e3ba2527805dcf4bf68a6abeecc71bcf9969
russell <rdpeake@adam.com.au>
2014-05-17 20:24:11 +0930

    Depth button and initial 'element' based design.

---
+++ Russell Peake Project/Russell Peake Project/Elements/Element1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Henge3D.Physics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Russell_Peake_Project.Elements
+{
+    class Element1 : Element
+    {
+        public Element1(Game1 game, PhysicsManager physics)
+            : base(game, physics)
+        {
+        }
+
+        public override void InitializeComponents()
+        {
+            SolidComponent sc = new SolidComponent(game, game.Content.Load<Model>("slab"), Color.Black.ToVector3());
+            sc.SetWorld(5, new Vector3(10, 5, 5), Quaternion.Identity);
+            Add(sc);
+
+        }
+    }
+}
---
+++ Russell Peake Project/Russell Peake Project/Elements/SolidComponent.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Henge3D;
+using Henge3D.Physics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Russell_Peake_Project.Interfaces;
+using IDrawable = Russell_Peake_Project.Interfaces.IDrawable;
+
+namespace Russell_Peake_Project.Elements
+{
+    public class SolidComponent : RigidBody, IComponent
+    {
+        Game1 Game;
+        Model model;
+        Vector3 color;
+
+        public SolidComponent(Game1 game, Model model, Vector3 color)
+            : base ((Henge3D.Pipeline.RigidBodyModel)model.Tag)
+        {
+            this.Game = game;
+            this.model = model;
+            this.color = color;
+            foreach (var mesh in model.Meshes)
+            {
+                foreach (BasicEffect effect in mesh.Effects)
+                {
+                    effect.EnableDefaultLighting();
+                    effect.AmbientLightColor = Vector3.One * 0.75f;
+                    effect.SpecularColor = Vector3.One;
+                    effect.PreferPerPixelLighting = true;
+                }
+            }
+        }
+
+        public void Draw(Matrix view)
+        {
+            foreach (var mesh in model.Meshes)
+            {
+                foreach (BasicEffect effect in mesh.Effects)
+                {
+                    effect.World = mesh.ParentBone.Transform * Transform.Combined;
+                    effect.View = view;
+                    effect.Projection = Game.ProjectionMatrix;
+                    effect.DiffuseColor = this.color;
+
+                }
+
+                mesh.Draw();
+            }
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            //do nothing
+        }
+    }
+}
