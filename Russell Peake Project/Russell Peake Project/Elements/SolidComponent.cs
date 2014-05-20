6ff5f6cc9b5d23a47eb5267ad47892f6d2f770be
russell <rdpeake@adam.com.au>
2014-05-20 21:20:58 +0930

    Pre-build autocommit: Build Successful

--- Russell Peake Project/Russell Peake Project/Elements/SolidComponent.cs
+++ Russell Peake Project/Russell Peake Project/Elements/SolidComponent.cs
@@ -24,51 +24,45 @@ namespace Russell_Peake_Project.Elements
             this.Game = game;
             this.model = model;
             this.color = color;
-            foreach (var mesh in model.Meshes)
-            {
-                foreach (BasicEffect effect in mesh.Effects)
-                {
-                    effect.EnableDefaultLighting();
-                    effect.AmbientLightColor = Vector3.One * 0.75f;
-                    effect.SpecularColor = Vector3.One;
-                    effect.PreferPerPixelLighting = true;
-
-                    effect.LightingEnabled = true; // turn on the lighting subsystem.
-                    effect.DirectionalLight0.DiffuseColor = new Vector3(0.5f, 0, 0); // a red light
-                    effect.DirectionalLight0.Direction = new Vector3(1, 0, 0);  // coming along the x-axis
-                    effect.DirectionalLight0.SpecularColor = new Vector3(0, 1, 0); // with green highlights
-                }
-            }
+            //foreach (var mesh in model.Meshes)
+            //{
+            //    foreach (Effect effect in mesh.Effects)
+            //    {
+            //        effect.EnableDefaultLighting();
+            //        effect.AmbientLightColor = Vector3.One * 0.75f;
+            //        effect.SpecularColor = Vector3.One;
+            //        effect.PreferPerPixelLighting = true;
+            //    }
+            //}
         }
 
         public void Draw(Matrix view)
         {
             foreach (var mesh in model.Meshes)
             {
-                foreach (BasicEffect effect in mesh.Effects)
+                foreach (Effect effect in mesh.Effects)
                 {
-                    //effect.CurrentTechnique = effect.Techniques["Simplest"];
-                    //effect.Parameters["xViewProjection"].SetValue(mesh.ParentBone.Transform * Transform.Combined * view * Game.ProjectionMatrix);
-                    //effect.Parameters["color"].SetValue(color.ToVector4());
+                    effect.CurrentTechnique = effect.Techniques["Simplest"];
+                    effect.Parameters["xViewProjection"].SetValue(mesh.ParentBone.Transform * Transform.Combined * view * Game.ProjectionMatrix);
+                    effect.Parameters["color"].SetValue(color.ToVector4());
 
-                    //effect.Parameters["xWorld"].SetValue(mesh.ParentBone.Transform * Transform.Combined);
-                    //effect.Parameters["xLightPos"].SetValue(Game1.lightPos);
-                    //effect.Parameters["xLightPower"].SetValue(Game1.lightPower);
-                    //effect.Parameters["xAmbient"].SetValue(Game1.ambientPower);
+                    effect.Parameters["xWorld"].SetValue(mesh.ParentBone.Transform * Transform.Combined);
+                    effect.Parameters["xLightPos"].SetValue(Game1.lightPos);
+                    effect.Parameters["xLightPower"].SetValue(Game1.lightPower);
+                    effect.Parameters["xAmbient"].SetValue(Game1.ambientPower);
 
-                    effect.World = mesh.ParentBone.Transform * Transform.Combined;
-                    effect.View = view;
-                    effect.Projection = Game.ProjectionMatrix;
-                    effect.DiffuseColor = this.color.ToVector3();
+                    //effect.World = mesh.ParentBone.Transform * Transform.Combined;
+                    //effect.View = view;
+                    //effect.Projection = Game.ProjectionMatrix;
+                    //effect.DiffuseColor = this.color;
 
-                    //foreach (EffectPass pass in effect.CurrentTechnique.Passes)
-                    //{
-                    //    pass.Apply();
-                    //    mesh.Draw();
-                    //}
+                    foreach (EffectPass pass in effect.CurrentTechnique.Passes)
+                    {
+                        pass.Apply();
+                        mesh.Draw();
+                    }
 
                 }
-                mesh.Draw();
             }
         }
 
