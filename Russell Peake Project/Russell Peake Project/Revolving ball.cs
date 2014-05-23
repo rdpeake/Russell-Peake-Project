11312d62f65b39645b11630fcf072ad29b26953c
rdpeake <rdpeake@adam.com.au>
2014-05-23 15:38:10 +0930

    Pre-build autocommit: Build Successful

--- Russell Peake Project/Russell Peake Project/Revolving ball.cs
+++ Russell Peake Project/Russell Peake Project/Revolving ball.cs
@@ -15,7 +15,7 @@ namespace Russell_Peake_Project
         public Revolving_ball(Game1 game) :
             base(game, Game1.models["sphere"], Color.Red)
         {
-            SetWorld(new Vector3(14f, 19f, 18f), Quaternion.Identity);
+            SetWorld(new Vector3(14f, 19.25f, 18f), Quaternion.Identity);
             //this.SetVelocity(10*Vector3.UnitY + 5 *Vector3.UnitX, Vector3.Zero);
         }
 
