f6490a7ac5dc163129330824f324ceb076213545
russell <rdpeake@adam.com.au>
2014-06-04 19:23:51 +0930

    Pre-build autocommit: Build Successful

--- Russell Peake Project/Russell Peake Project/Game1.cs
+++ Russell Peake Project/Russell Peake Project/Game1.cs
@@ -108,7 +108,7 @@ namespace Russell_Peake_Project
             //initialise models
             Effect e = Content.Load<Effect>("effect1");
             Game1.models = new Dictionary<string, Model>();
-            foreach (string s in "sphere,slab,rail".Split(','))
+            foreach (string s in "sphere,slab,rail,obelisk".Split(','))
             {
                 Model model = Content.Load<Model>(s);
                 foreach (ModelMesh mesh in model.Meshes)
